73ca3f47656ef4983c2c43a9353829dbf5c4b2cc
harshitadhage <100786569+harshitadhage@users.noreply.github.com>
2022-04-21 22:03:57 +0530

    Added files

---
+++ navbar.php
@@ -0,0 +1,10 @@
+<nav style="background-color:#8A39E1;" class="navbar sticky-top navbar-expand-md navbar-dark">
+	<a class="navbar-brand" href="index.php">💵All India Bank💵</a>
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar"><span class="navbar-toggler-icon"></span></button>
+	<div class="collapse navbar-collapse" id="collapsibleNavbar">
+		<ul class="navbar-nav ml-auto">
+			<li class="nav-item"><a class="nav-link" href="index.php"><span style="text: size 15em;">Home</span></a></li>
+			<li class="nav-item"><a class="nav-link" href="transactionhistory.php">Transaction History</a></li>
+		</ul>
+	</div>
+</nav>
